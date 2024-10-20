425a09fd22f8116415596cc279d0052e99795e23
Nikita Polev <nikitko779@gmail.com>
2024-10-20 04:17:53 +0300

    реализовал слайдер 'Выгода для клиентов'

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import scroll from './modules/scroll'
 import form from './modules/forms'
 import calculator from './modules/calculator'
 import timer from './modules/timer'
-
+import slider from './modules/slider'
 
 orderCall()
 scroll()
 form()
 calculator()
 timer('30 october 2024 12:45:00')
+slider()
---
+++ src/modules/slider.js
@@ -0,0 +1,95 @@
+const slider = () => {
+    const sliderBlock = document.querySelector('#benefits')
+    const slides = document.querySelectorAll('.benefits__item')
+    const timeInterval = 2000
+
+    let slideCount = 0
+    let currentSlide = 0
+
+    let interval
+
+    const slideShow = () => {
+        slides.forEach((item, index) => {
+            item.classList.remove('benefits_item-active')
+            if (window.innerWidth >= 576) {
+                slideCount = 3
+                if (index >= currentSlide && index < currentSlide + slideCount) {
+                    item.style.display = "block"
+                } else {
+                    item.style.display = "none"
+                }
+            } else {
+                slideCount = 1
+                if (index === currentSlide) {
+                    item.style.display = "block"
+                } else {
+                    item.style.display = "none"
+                }
+            }
+        })
+    }
+
+    const prevSlide = (elems, index, strClass) => {
+        elems[index].classList.remove(strClass)
+        slideShow()
+    }
+    const nextSlide = (elems, index, strClass) => {
+        elems[index].classList.add(strClass)
+        slideShow()
+    }
+
+    const autoSlide = () => {
+        prevSlide(slides, currentSlide, 'benefits__item-active')
+        currentSlide += slideCount
+
+        if (currentSlide >= slides.length) {
+            currentSlide = 0
+        }
+        nextSlide(slides, currentSlide, 'benefits__item-active')
+        slideShow()
+    }
+
+    const startSlide = (timer = 1500) => {
+        interval = setInterval(autoSlide, timer)
+    }
+    const stopSlide = () => {
+        clearInterval(interval)
+    }
+
+    sliderBlock.addEventListener('click', (e) => {
+        e.preventDefault()
+
+        prevSlide(slides, currentSlide, 'benefits__item-active')
+        if (e.target.closest('.benefits__arrow--left')) {
+            currentSlide -= slideCount
+
+
+        } else if (e.target.closest('.benefits__arrow--right')) {
+            currentSlide += slideCount
+
+        }
+        if (currentSlide >= slides.length) {
+            currentSlide = 0
+        }
+        if (currentSlide < 0) {
+            currentSlide = slides.length - slideCount
+        }
+        nextSlide(slides, currentSlide, 'benefits__item-active')
+    })
+    sliderBlock.addEventListener('mouseenter', (e) => {
+        if (e.target.matches('.benefits-arrows')) {
+            stopSlide()
+        }
+    }, true)
+    sliderBlock.addEventListener('mouseleave', (e) => {
+        if (e.target.matches('.benefits-arrows')) {
+            startSlide(timeInterval)
+        }
+    }, true)
+
+    slideShow()
+    startSlide(timeInterval)
+
+};
+
+export default slider
